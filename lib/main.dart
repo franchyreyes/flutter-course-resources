d298b60c0ed91476231d515dccf510e3a3d2c6eb
franchycloud100 <franchyreyes@hotmail.com>
2019-10-25 16:48:36 -0400

    Add personal card with card, ListTitle,icons and Text

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class MyApp extends StatelessWidget{
           backgroundColor: Colors.teal,
           body:  SafeArea(
                 child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
                   children: <Widget>[
                     CircleAvatar(
                       radius: 50.0,
@@ -35,54 +36,49 @@ class MyApp extends StatelessWidget{
                           fontWeight: FontWeight.bold
                       ),
                     ),
-                    Container(
-                      padding: EdgeInsets.all(10.0),
+                    SizedBox(
+                      height: 20.0,
+                      width: 100.0,
+                      child: Divider(
+                        color: Colors.teal.shade100,
+                      ),
+                    ),
+                    Card(
                       color: Colors.white,
                       margin: EdgeInsets.symmetric(vertical: 10.0,horizontal: 20.0),
-                      child: Row(
-                        children: <Widget>[
-                          Icon(
-                            Icons.phone,
-                            color: Colors.teal,
-                          ),
-                          SizedBox(
-                            width: 10.0,
-                          ),
-                          Text(
+                      child: ListTile(
+                        leading: Icon(
+                          Icons.phone,
+                          color: Colors.teal,
+                        ),
+                        title: Text(
                             '829-xxx-xxxx',
                             style: TextStyle(
                               color: Colors.teal.shade100,
                               fontFamily: 'SourceSansPro',
                               fontSize: 20.0,
                             ),
-                          )
-                        ],
-                      ),
+                        ),
+                      )
                     ),
-                    Container(
-                      padding: EdgeInsets.all(10.0),
-                      color: Colors.white,
-                      margin: EdgeInsets.symmetric(vertical: 10.0,horizontal: 20.0),
-                      child: Row(
-                        children: <Widget>[
-                          Icon(
+                    Card(
+                        color: Colors.white,
+                        margin: EdgeInsets.symmetric(vertical: 10.0,horizontal: 20.0),
+                        child: ListTile(
+                          leading: Icon(
                             Icons.email,
                             color: Colors.teal,
                           ),
-                          SizedBox(
-                            width: 10.0,
-                          ),
-                          Text(
+                          title: Text(
                             'franchyjavier@gmail.com',
                             style: TextStyle(
                               color: Colors.teal.shade100,
                               fontFamily: 'SourceSansPro',
                               fontSize: 20.0,
                             ),
-                          )
-                        ],
-                      ),
-                    )
+                          ),
+                        )
+                    ),
                   ],
                 ),
           ),
